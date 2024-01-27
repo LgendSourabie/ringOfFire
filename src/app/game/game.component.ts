d0aee60b075cae5d7ad6ae04b8a5145502d3d3c1
Ibrahima SOURABIE <ge65yub@mytum.de>
2024-01-27 20:18:58 +0100

    card animation implemented

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { NgStyle } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [NgFor, NgStyle],
+  imports: [NgFor, NgStyle, NgIf],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
